3f30e9f546008e36f31fc4808f47f5d82882c970
leonid460 <leonid.kol4in@gmail.com>
2019-02-11 19:40:01 +0300

    Added: "about" page, link to "about" in "index", link to "index" in "about"

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+#def angel(request):
+#    return render(request, 'index.html', {})
+
 def index(request):
-    return HttpResponse("Rango says hey there partner!")
+    return HttpResponse('''Rango says hey there partner!
+    <br/><a href='rango/about'>About</a>''')
+
+
+def about(request):
+    return HttpResponse('''Rango says here is the about page
+    <br/><a href="/rango/">Back to index page</a>''')
--- tango_with_django_project/tango_with_django_project/urls.py
+++ tango_with_django_project/tango_with_django_project/urls.py
@@ -19,6 +19,7 @@ from django.conf.urls import include
 from rango import views
 
 urlpatterns = [
+    #url(r'^angel$',views.angel, name='angel'),
     url(r'^$',views.index, name='index'),
     url(r'^rango/', include('rango.urls')),
     # above maps any URLs starting
