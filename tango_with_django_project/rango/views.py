0b719f9549b4dba362a06d1c882b3f3b81313df7
leonid460 <leonid.kol4in@gmail.com>
2019-03-23 19:09:37 +0300

    Added categries on main page.

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -1,12 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from rango.models import Category
 
 #def angel(request):
 #    return render(request, 'index.html', {})
 
 def index(request):
-    context_dict = {'boldmessage': "Crunchy, creamy, cookie, candy, cupcake!"}
-    return render(request, 'rango/index.html', context=context_dict)
+    # context_dict = {'boldmessage': "Crunchy, creamy, cookie, candy, cupcake!"}
+    category_list = Category.objects.order_by('-likes')[:5]
+    context_dict = {'categories': category_list}
+
+    return render(request, 'rango/index.html', context_dict)
 
 def about(request):
     context_dict = {'boldmessage': "This is about page"}
