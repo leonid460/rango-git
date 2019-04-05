0873e35678808ba198e8ca7b26e479dbf30969ce
leonid460 <leonid.kol4in@gmail.com>
2019-04-05 20:15:05 +0300

    Added 'Most Liked Categories' and 'Most Viewed Pages', added back from category page, added forms 'CategoryForm' & 'PageForm' with templates & views, added 'add_category' page, modifyed the index page template.

--- tango_with_django_project/rango/urls.py
+++ tango_with_django_project/rango/urls.py
@@ -1,7 +1,16 @@
-from django.conf.urls import url
+from django.conf.urls import patterns, url
 from rango import views
 
 urlpatterns = [
 	url(r'^$', views.index, name='index'),
 	url(r'^about/$',views.about, name='about'),
+	url(r'^add_category/$', views.add_category, name='add_category'),
+	url(r'^category/(?P<category_name_slug>[\w\-]+)/$',
+	views.show_category, name='show_category'),
 ]
+
+#urlpatterns = patterns('',
+#	url(r'^$', views.index, name='index'),
+#	url(r'^about/$', views.about, name='about'),
+#	url(r'^category/(?P<category_name_slug>[\w\-]+)/$',
+#	views.show_category, name='show_category'),)
--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -1,17 +1,46 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from rango.models import Category
+from rango.models import Page
+from rango.forms import CategoryForm
 
 #def angel(request):
 #    return render(request, 'index.html', {})
 
 def index(request):
-    # context_dict = {'boldmessage': "Crunchy, creamy, cookie, candy, cupcake!"}
     category_list = Category.objects.order_by('-likes')[:5]
-    context_dict = {'categories': category_list}
-
+    page_list = Page.objects.order_by('-views')[:5]
+    context_dict = {'categories': category_list, 'pages': page_list}
     return render(request, 'rango/index.html', context_dict)
 
 def about(request):
     context_dict = {'boldmessage': "This is about page"}
     return render(request, 'rango/about.html', context=context_dict)
+
+def show_category(request, category_name_slug):
+    context_dict = {}
+    try:
+        category = Category.objects.get(slug=category_name_slug)
+        pages = Page.objects.filter(category=category)
+        context_dict['pages'] = pages
+        context_dict['category'] = category
+    except Category.DoesNotExist:
+        context_dict['category'] = None
+        context_dict['pages'] = None
+    return render(request, 'rango/category.html', context_dict)
+
+def add_category(request):
+    form = CategoryForm()
+    # HHTP post?
+    if request.method == 'POST':
+        form = CategoryForm(request.POST)
+        # Have we been provided with a valid form?
+        if form.is_valid():
+            # Save the new category to the database
+            form.save(commit=True)
+            # Direct the user back to the index page
+            return index(request)
+        else:
+            # Print errors to terminal
+            print(form.errors)
+    return render(request, 'rango/add_category.html', {'form': form})
